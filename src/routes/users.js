a4ade95e588778cffadbc2760cae8fe888fcaaa8
Aanand <akgit2000@gmail.com>
2021-01-01 20:26:41 +0530

    core security changed

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,13 +1,1 @@
-const express  = require('express');
-
-const router = express.Router();
-
-router.get('/' , (req , res)=>{
-    res.send(`Hello`)
-})
-
-router.post('/register' , (req , res)=>{
-    console.log(req.body)
-    res.send("Hello")
-})
-module.exports = router;
+ 
